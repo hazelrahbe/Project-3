aa62ed962f0b10d9047a2531a5f557c2a255efd2
hazelrahbe <swp.hazel94@gmail.com>
2022-09-17 16:20:27 -0400

    Header, Nav and Footer set up

--- cryptoproject_frontend/src/App.js
+++ cryptoproject_frontend/src/App.js
@@ -1,12 +1,18 @@
 import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
 import './App.css';
+import Footer from './components/Footer';
 
-function App(props) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Nav />
+        <Main />
       </header>
+        <Footer />
     </div>
   );
 }
