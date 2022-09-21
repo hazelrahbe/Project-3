9cbaf157c96ace28edaca0305a393caef4f53d95
audriana1126 <audriana1126@yahoo.com>
2022-09-21 18:09:04 -0400

    css

--- cryptoproject_frontend/src/components/Footer.js
+++ cryptoproject_frontend/src/components/Footer.js
@@ -5,10 +5,12 @@ const Footer = (props) => {
     return (
         <div className="footer">
             <Link style={{textDecoration: 'none'}} to="/aboutus">
-            <div class="whiteP"><p>About Us</p></div>
+            <div className="footerdiv">
+            <div class="whiteP"><p>About Us</p></div></div>
             </Link>
             <Link style={{textDecoration: 'none'}} to="/privacy">
-            <div class="whiteP"><p>Privacy Policy</p></div>
+                <div className="footerdiv">
+            <div class="whiteP"><p>Privacy Policy</p></div></div>
             </Link>
         </div>
     )
