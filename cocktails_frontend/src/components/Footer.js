a2821043db9fb5f2d094b2896a8e0c1d5012f30e
audriana1126 <audriana1126@yahoo.com>
2022-09-22 11:45:58 -0400

    testing footer

--- cocktails_frontend/src/components/Footer.js
+++ cocktails_frontend/src/components/Footer.js
@@ -5,10 +5,14 @@ const Footer = (props) => {
     return (
         <div className="footer">
             <Link style={{textDecoration: 'none'}} to="/aboutus">
+            <div className="footerdiv">
             <div class="whiteP"><p>About Us</p></div>
+            </div>
             </Link>
             <Link style={{textDecoration: 'none'}} to="/privacy">
+            <div className="footerdiv">
             <div class="whiteP"><p>Privacy Policy</p></div>
+            </div>
             </Link>
         </div>
     )
